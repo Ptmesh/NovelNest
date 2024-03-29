57bcd2ee27f285870a3e0d415f58c81678a4e78a
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-03-29 12:45:39 +0530

    Added the delete functionality

--- frontend/components/DeleteBook.jsx
+++ frontend/components/DeleteBook.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DeleteBook = () => {
-  return <div className="delete">DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleDelete = () => {
+    setLoading(false);
+    axios
+      .delete(`http://localhost:3000/books/${id}`)
+      .then((res) => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something Went Wrong, Please try again later!");
+        setLoading(false);
+      });
+  };
+  return (
+    <div className="delete">
+      <h1>Delete Book</h1>
+      {loading ? (
+        <h1>Please Wait</h1>
+      ) : (
+        <div>
+          <h3>Are you sure you want to delete this book?</h3>
+          <button className="btn-delete" onClick={handleDelete}>
+            Yes Go Ahead <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DeleteBook;
